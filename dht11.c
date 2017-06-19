b723763d82061e6176217e3e0e73f6fca7de86ca
Sergey <brigadauh@mail.ru>
2017-06-18 23:01:51 -0400

    write to file and db

--- dht11.c
+++ dht11.c
@@ -12,8 +12,8 @@ void writeData(int h0, int h1, int t0, int t1) {
 	int status;
 	time_t t = time(NULL);
 	struct tm tm = *localtime(&t);
-	sprintf(data_str,"curl -X POST -H \"Content-Type: application/json\"  -d '{\"t\":\"%d.%d\",\"h\":\"%d.%d\",\"d\":\"%d-%d-%d %d:%d:%d\"}' http://192.168.1.3:8888/api/weather/temphumidity/add\n", t0,t1,h0,h1, tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
-	status = system(data_str);
+	/*sprintf(data_str,"curl -X POST -H \"Content-Type: application/json\"  -d '{\"t\":\"%d.%d\",\"h\":\"%d.%d\",\"d\":\"%d-%d-%d %d:%d:%d\"}' http://192.168.1.3:8888/api/weather/temphumidity/add\n", t0,t1,h0,h1, tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+	status = system(data_str);*/
 	if (status!=0) {
 	  FILE *f = fopen("data.json", "a");
 	  if (f == NULL)
@@ -25,6 +25,53 @@ void writeData(int h0, int h1, int t0, int t1) {
 	  fclose(f);
 	}
 }
+int read_and_remove_Line() {
+	char* inFileName = "data.json";
+	char* outFileName = "tmp.json";
+	FILE* inFile = fopen(inFileName, "r");
+	FILE* outFile = fopen(outFileName, "a");
+ 	char *data_str = (char*)malloc(255 * sizeof(char));
+	int status;
+	char line [1024]; // maybe you have to use better value here
+	int lineCount = 0;
+	int ret = -1;
+
+	if( inFile == NULL )
+	{
+	    printf("Open Error");
+	}
+	
+	while( fgets(line, sizeof(line), inFile) != NULL )
+	{
+	    if(lineCount == 0){
+		sprintf(data_str,"curl -X POST -H \"Content-Type: application/json\"  -d '%s' http://192.168.1.3:8888/api/weather/temphumidity/add\n", line);
+		status = system(data_str);
+		if (status==0) {
+		   ret=0;
+		}
+	    }
+	    else
+	    {
+	        fprintf(outFile, "%s", line);
+	    }
+	
+	    lineCount++;
+	}
+	
+	
+	fclose(inFile);
+	fclose(outFile);
+	
+	// possible you have to remove old file here before
+	printf("renaming...");
+	if( !rename(outFileName, inFileName) )
+	{
+	    printf("Rename Error");
+	}
+	return ret;
+}
+
+
 void read_dht11_dat()
 {
 	uint8_t laststate	= HIGH;
@@ -74,12 +121,14 @@ void read_dht11_dat()
 		printf( "Humidity = %d.%d %% Temperature = %d.%d C (%.1f F)\n",
 			dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3], f );
 		writeData(dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3]);
+		while (read_and_remove_Line()==0) {}
 		exit(0);
 		
 	}else  {
 		printf( "Data not good, skip\n" );
 	}
 }
+
  
 int main( void )
 {
